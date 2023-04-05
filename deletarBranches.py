6c767f1850811876d82ced5038f6980fac771903
ViniciusPedroza <viniciuspdelsin@hotmail.com>
2023-04-05 15:17:43 -0300

    Mudança no deletarBranchs

--- deletarBranches.py
+++ deletarBranches.py
@@ -7,7 +7,7 @@ while i <= 100:
         print('Branch numero: ' + str(i))
         
         # Remove local Branch
-        subprocess.run('git branch -D branch_' + str(i))
+        subprocess.run('git branch -D branch_' + str(i) + 'k')
         
         #Remove remote Branch
         #subprocess.run('git push origin -d branch_' + str(i))
